5f77b3fd2b551206910f52575bd4baa1a763f51e
Wakana <pre.kuro.05@gmail.com>
2025-02-17 16:44:23 +0100

    making so it will show text in cv education

--- CV/CV.js
+++ CV/CV.js
@@ -16,6 +16,76 @@ function fetchData(){
     document.getElementById("email").innerHTML='<i class="fa-solid fa-envelope"></i> '+data.contact_info.email;
     document.getElementById("address").innerHTML='<i class="fa-solid fa-house"></i> '+data.contact_info.address;
 
+    let count = data.education.count;
+    let educationList = data.education;
+    
+    const tbl= document.getElementById("educationList");
+
+    /** reference att create automatik table  https://qiita.com/forever---searcher/items/7901217dc811d72687f8 */
+    
+    if(count >0){
+
+      for(let x; x<count; x++){
+        let tr = document.createElement("tr");
+
+        foreach((educationList)=>{
+
+            // how to add attribute https://www.quora.com/Can-you-create-an-element-in-JS-with-an-ID-or-a-class
+        let tdDuration = createElement("td");
+        tdDuration.setAttribute("class","duration");
+
+        let tdschool = createElement("td");
+        tdschool.setAttribute("class","school");
+
+        let tdcourse = createElement("td");
+        tdcourse.setAttribute("class","course");
+        
+        let tdtopics = createElement("td");
+        tdtopics.setAttribute("class","topics");
+        tdtopics.setAttribute("id","topic")
+
+        let topicsCount =    data.education.topics.count;
+       
+        if(topicsList >0){
+          
+          let ul = document.createElement("ul");
+          let list= data.education.topics;
+          foreach((list )=>{
+              let li =createElement("li");
+              document.create
+
+
+          })
+
+
+
+        }
+      
+
+
+
+
+
+
+      })
+
+    
+
+        
+
+
+      }
+       
+        
+
+     
+
+
+      );
+
+    }
+
+
   }
 )
 }

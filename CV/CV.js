32563095bbbf7810bfd371f130dd0e770651d6ad
Wakana <pre.kuro.05@gmail.com>
2025-02-18 10:43:02 +0100

    changed cv parts to json

--- CV/CV.js
+++ CV/CV.js
@@ -1,82 +1,99 @@
-function fetchData(){
-
+function fetchData() {
   fetch("cv.json")
-  .then(function (response) {
+    .then(response => response.json())
+    .then(data => {
+      // set up data for profile
+      document.getElementById("profile_pic").src = data.profile_pic;
+      document.getElementById("fullname").innerHTML = data.name;
+      document.getElementById("curentTitle").innerHTML = data.title;
+      document.getElementById("phone").innerHTML =
+        '<i class="fa-solid fa-phone"></i> ' + data.contact_info.phone;
+      document.getElementById("email").innerHTML =
+        '<i class="fa-solid fa-envelope"></i> ' + data.contact_info.email;
+      document.getElementById("address").innerHTML =
+        '<i class="fa-solid fa-house"></i> ' + data.contact_info.address;
 
-    return response.json();
 
-  }).then(function (data){
+      //set Education information
+      // get Education List 
+      let educationList = data.education;
+      const tbl = document.getElementById("tb_education");
 
-    document.getElementById("profile_pic").src = data.profile_pic;
-    document.getElementById("fullname").innerHTML=data.name;
-    document.getElementById("curentTitle").innerHTML=data.title;
-    document.getElementById("phone").innerHTML='<i class="fa-solid fa-phone"></i> '+data.contact_info.phone;
-    document.getElementById("email").innerHTML='<i class="fa-solid fa-envelope"></i> '+data.contact_info.email;
-    document.getElementById("address").innerHTML='<i class="fa-solid fa-house"></i> '+data.contact_info.address;
+      /** reference att create table  
+       * https://qiita.com/forever---searcher/items/7901217dc811d72687f8 
+       * https://qiita.com/forever---searcher/items/7caf122d1b78bd70ebec
+       * https://tcd-theme.com/2022/07/javascript-dom-textcontent.html
+       */
+      if (educationList.length > 0) {
+        educationList.forEach(edu => {
+          let tr = document.createElement("tr");
 
-    let count = data.education.count;
-    let educationList = data.education;
-    
-    const tbl= document.getElementById("educationList");
+          createtd(tr, edu.duration);
+          createtd(tr, edu.school);
+          createtd(tr, edu.course);
 
-    /** reference att create automatik table  https://qiita.com/forever---searcher/items/7901217dc811d72687f8 */
-    
-    if(count >0){
-
-      forEach(edu in educationList){
-        let tr = document.createElement("tr");
-
-            createElement("duration",tr,edu.duration);
-            createElement("school",tr,text);
-            createElement("course",tr,edu.topics);
-            createElement("topics",tr,edu.topics);
-           
-
-        let topicsCount =    data.education.topics.count;
+          let tdTopics = document.createElement("td");
        
-        if(topicsCount >0){
-          
-          let ul = document.createElement("ul");
-          ul.setAttribute("id","ul_topics");
 
-          let list= edu.topics;
-          
-          foreach((list )=>{
-              let li =createElement("li");
-              li.setAttribute("id",li_topics);
-              li.textContent=list.topics;
+          //  add topic lista
+          if (edu.topics && edu.topics.length > 0) {
+            let ul = document.createElement("ul");
+
+            edu.topics.forEach(topic => {
+              let li = document.createElement("li");
+              li.textContent = topic;
+              ul.appendChild(li);
+            });
+
+            tdTopics.appendChild(ul);
+          }
+
+          tr.appendChild(tdTopics);
+          tbl.appendChild(tr);
+        });
+      }
+
+      //set Work Experience
+      const workExList= data.work_experience;
+      const tb_work =document.getElementById("tb_work");
+      
+      workExList.forEach(work=>{
+       const tr_work = document.createElement("tr");
+       createtd(tr_work,work.duration);
+       createtd(tr_work,work.company);
+       createtd(tr_work,work.title);
+       let tdResp = document.createElement("td");
+      
+       
+       if(work.responsibilities.length>0){
+        let workul=document.createElement("ul")
+        work.responsibilities.forEach(res =>{
         
-              ul.append(li);
-      
+          let work_li = document.createElement("li");
+          work_li.textContent = res;
+          workul.appendChild(work_li);
 
-          })
+        });
 
-
-
-        }
-      
-
-
-
+        tdResp.appendChild(workul);
+       }
+        tr_work.appendChild(tdResp);
+        tb_work.append(tr_work);
 
 
-
-        })
-
-    
-
-        
+      })
 
 
-        };
-      
 
 
-function createElement(id,tr,text){
-  let td = createElement("td");
-  td.setAttribute("class",id);
-  td.setAttribute("id",id);
-  td.textContent=text;
-  tr.append(td);
 
+    }) //reference https://zenn.dev/junki555/articles/4ab67fc78ce64c
+    .catch(error => console.error("Error fetching data:", error));
+}
+
+// function for create table
+function createtd(tr, text) {
+  let td = document.createElement("td");
+  td.textContent = text;
+  tr.appendChild(td);
 }
